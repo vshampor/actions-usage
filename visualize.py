6faf0bfb9b39afabb4002770fd4e008c8bd442a9
Vasily Shamporov <vasily.shamporov@intel.com>
2023-10-09 23:33:15 +0200

    Better aspect

--- visualize.py
+++ visualize.py
@@ -6,6 +6,8 @@ import sys
 input_filename = Path(sys.argv[1])
 df = pd.read_csv(input_filename)
 df[df.columns[0]] = pd.to_datetime(df[df.columns[0]], unit='s')
+w, h = plt.figaspect(1/3)
+plt.figure(figsize=(w, h))
 plt.xlabel('Date/Time')
 plt.ylabel('Concurrent GH action jobs')
 plt.plot(df[df.columns[0]], df[df.columns[1]])
